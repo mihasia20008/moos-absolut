cd4e025020fe710395ac279eee744e796d7851be
Alexander Mikhaylov <aleksandr.mikhaylov@onetwotrip.com>
2019-06-23 19:45:57 +0300

    fix form

--- src/components/CustomDetail/TextField/index.js
+++ src/components/CustomDetail/TextField/index.js
@@ -1,37 +1,19 @@
-import React, {Fragment, PureComponent} from 'react';
+import React, {Fragment} from 'react';
 import cx from 'classnames';
 
-import ClearButton from '../../ClearButton';
-
-class TextField extends PureComponent {
-  handleClearField = () => {
-    const { input: { onChange } } = this.props;
-    onChange('');
-  };
-
-  render() {
-    const {input: { value, ...inputProps }, label, disabled, defaultValue} = this.props;
-    const isHiddenClearButton = Boolean(!value || disabled);
-
-    return (
-      <Fragment>
-        <label>{label}</label>
-        <div className={cx('task-form__input-wrap')}>
-          <input
-            type="text"
-            className={cx('center')}
-            disabled={disabled}
-            value={value || defaultValue}
-            {...inputProps}
-          />
-          <ClearButton
-            onClear={this.handleClearField}
-            isHidden={isHiddenClearButton}
-          />
-        </div>
-      </Fragment>
-    );
-  }
-}
+const TextField = ({input: { value, ...inputProps }, label, disabled, defaultValue}) => (
+  <Fragment>
+    <label>{label}</label>
+    <div className={cx('task-form__input-wrap')}>
+      <input
+        type="text"
+        className={cx('center')}
+        disabled={disabled}
+        value={value || defaultValue}
+        {...inputProps}
+      />
+    </div>
+  </Fragment>
+);
 
 export default TextField;
